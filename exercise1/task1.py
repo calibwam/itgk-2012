60b40ff26c8f34f315fceb6a45efc57b112c29be
Péter Gombos <gombos@starbuck.(none)>
2012-08-31 18:02:21 +0200

    added task1

---
+++ exercise1/task1.py
@@ -0,0 +1,17 @@
+import math
+pi = 3.1415
+
+x1 = 3 * math.cos(pi / 2)
+y1 = 3 * math.sin(pi / 2)
+
+print("1: (%f,%f)" % (x1, y1))
+
+x2 = 2.3 * math.cos(pi / 3)
+y2 = 2.3 * math.sin(pi / 3)
+
+print("2: (%f,%f)" % (x2, y2))
+
+x3 = 5 * math.cos(0)
+y3 = 5 * math.sin(0)
+
+print("3: (%f,%f)" % (x3, y3))
