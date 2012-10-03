a853c11b9f967e98094f1d9e7904c8250db9f67a
Péter Gombos <gombos@stud.ntnu.no>
2012-10-03 09:57:47 +0200

    added workdays, still buggy

---
+++ exercise5/workdays.py
@@ -0,0 +1,15 @@
+days = [0,'man','tue','wed','thu','fri','sat','sun']
+def weekday_newyear(year):
+    day = 1
+    for x in range(1900,year):
+        if ((year % 4 == 0) or ((year % 100 == 0) and (year % 400 !=0))):
+            day += 2
+        else:
+            day +=1
+    return day % 7
+
+def is_workday(weekday):
+    pass
+
+for a in range(1900,1920):
+    print(a, days[weekday_newyear(a)])
